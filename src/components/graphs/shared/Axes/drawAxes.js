b7a1382c2b62f7615520cb0ad10662021b4bd108
arturomp99 <arturo.m.p.99@gmail.com>
2023-09-08 18:03:00 +0200

    fix: moved horizontal axis to bottom

--- src/components/graphs/shared/Axes/drawAxes.js
+++ src/components/graphs/shared/Axes/drawAxes.js
@@ -24,8 +24,12 @@ export const createAxes = (parentRef, xScale, yScale) => {
 export const giveSizeToAxes = (parentRef, xScale, yScale, width, height) => {
   xScale.range([0, width]);
   yScale.range([0, height]);
+
   const xAxis = d3.axisBottom(xScale).ticks(4);
   const yAxis = d3.axisLeft(yScale).tickSize(4);
+
   d3.select(parentRef).select("#hAxis").call(xAxis);
   d3.select(parentRef).select("#vAxis").call(yAxis);
+
+  d3.selectAll("#hAxis").attr("transform", `translate(0,${height})`);
 };
--- src/data/constants.js
+++ src/data/constants.js
@@ -1,1 +1,3 @@
 export const cardResizeTimeout = 1000;
+export const fontSize = 10;
+export const graphMargin = { top: 10, bottom: 10, left: 10, right: 10 };
