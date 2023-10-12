44835c0003a43a8bb668ec0aee37d665f97fc870
arturomp99 <arturo.m.p.99@gmail.com>
2023-10-13 01:01:04 +0200

    feat: adapt scales to available data

--- src/components/graphs/LineChart/useLineChartScales.js
+++ src/components/graphs/LineChart/useLineChartScales.js
@@ -1,11 +1,27 @@
 import * as d3 from "d3";
 import { useEffect, useRef } from "react";
 
-export const useLineChartScales = () => {
-  let scales = [
-    d3.scaleTime().domain([0, 10]),
-    d3.scaleLinear().domain([10, 0]),
-  ];
+export const useLineChartScales = (data) => {
+  let xScale = d3.scaleTime().domain([0, 10]);
+  let yScale = d3.scaleLinear().domain([10, 0]);
 
-  return scales;
+  useEffect(() => {
+    if (!data) {
+      // DEFAULT VALUES
+      xScale.domain([0, 10]);
+      yScale.domain([10, 0]);
+    } else {
+      console.log("data", data);
+      xScale.domain([
+        d3.min(data, (dataInstance) => dataInstance.timeStamp),
+        d3.max(data, (dataInstance) => dataInstance.timeStamp),
+      ]);
+      yScale.domain([
+        d3.min(data, (dataInstance) => dataInstance.soundMax),
+        d3.max(data, (dataInstance) => dataInstance.soundMax),
+      ]);
+    }
+  }, [data]);
+
+  return [xScale, yScale];
 };
