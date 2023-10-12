879339941af3f09d7d8f39232095ac00c0d38de5
arturomp99 <arturo.m.p.99@gmail.com>
2023-10-13 01:57:05 +0200

    fix: scale is lost on resize

--- src/components/graphs/LineChart/useLineChartScales.js
+++ src/components/graphs/LineChart/useLineChartScales.js
@@ -1,25 +1,24 @@
 import * as d3 from "d3";
-import { useEffect, useRef } from "react";
+import { useEffect, useState } from "react";
 
 export const useLineChartScales = (data) => {
-  let xScale = d3.scaleTime().domain([0, 10]);
-  let yScale = d3.scaleLinear().domain([10, 0]);
+  let [[xScale, yScale], setScales] = useState([
+    d3.scaleTime().domain([0, 10]),
+    d3.scaleLinear().domain([10, 0]),
+  ]);
 
   useEffect(() => {
     if (!data) {
-      // DEFAULT VALUES
-      xScale.domain([0, 10]);
-      yScale.domain([10, 0]);
-    } else {
-      xScale.domain([
-        d3.min(data, (dataInstance) => dataInstance.timeStamp),
-        d3.max(data, (dataInstance) => dataInstance.timeStamp),
-      ]);
-      yScale.domain([
-        d3.max(data, (dataInstance) => dataInstance.soundMax),
-        d3.min(data, (dataInstance) => dataInstance.soundMax),
-      ]);
+      return;
     }
+    let minX = d3.min(data, (dataInstance) => dataInstance.timeStamp);
+    let maxX = d3.max(data, (dataInstance) => dataInstance.timeStamp);
+    let minY = d3.max(data, (dataInstance) => dataInstance.soundMax);
+    let maxY = d3.min(data, (dataInstance) => dataInstance.soundMax);
+    setScales(([prevXScale, prevYScale]) => [
+      prevXScale.domain([minX, maxX]),
+      prevYScale.domain([minY, maxY]),
+    ]);
   }, [data]);
 
   return [xScale, yScale];
