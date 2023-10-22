d3abbf89ab00959acff55512ad40a2866bd1d8f9
arturomp99 <arturo.m.p.99@gmail.com>
2023-10-22 21:12:47 +0200

    feat: create lines for linechart

--- src/components/graphs/LineChart/drawLines.ts
+++ src/components/graphs/LineChart/drawLines.ts
@@ -1,10 +1,25 @@
 import * as d3 from "d3";
 import { LineChartDataType } from "./lineChart.types";
-// import { graphMargin } from "../../../data/constants";
+import { graphMargin } from "../../../data/constants";
 
 export const drawLines = (
   parentRef: SVGSVGElement | null,
   data: LineChartDataType[]
 ) => {
-  d3.select(parentRef).append("path").datum(data).attr("fill", "none");
+  const line = d3
+    .line<LineChartDataType>()
+    .x((dataPoint) => {
+      return dataPoint.scaledX;
+    })
+    .y((dataPoint) => dataPoint.scaledY);
+
+  d3.select(parentRef)
+    .selectAll(".dataLine")
+    .data([data])
+    .join("path")
+    .attr("d", line)
+    .attr("class", "dataLine")
+    .attr("fill", "none")
+    .attr("stroke", "black")
+    .attr("transform", `translate(${graphMargin.left},${graphMargin.top})`);
 };
