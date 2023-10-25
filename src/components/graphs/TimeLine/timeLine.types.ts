2c4f50b5fab5e17bfcf184970823c86aee26e745
arturomp99 <arturo.m.p.99@gmail.com>
2023-10-25 02:02:31 +0200

    feat: TIMELINE draw markers

--- src/components/graphs/TimeLine/timeLine.types.ts
+++ src/components/graphs/TimeLine/timeLine.types.ts
@@ -1,5 +1,14 @@
-export interface TimeLineDataType {
+export interface TemporalDataType {
   name: string;
   start_time: string;
   finish_time: string;
+}
+
+export interface TimeLineChartDataType {
+  key: string;
+  scaledX: number;
+  scaledY: number;
+  width: number;
+  // function to get the marker height as a function of the total graph height
+  getHeight: (graphHeight: number) => number;
 }
