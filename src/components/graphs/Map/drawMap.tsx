5e520188d6940ab7ecc55f78a164182cae74cca8
arturomp99 <arturo.m.p.99@gmail.com>
2023-12-02 17:27:39 +0100

    fix: MAP extended map types

--- src/components/graphs/Map/drawMap.tsx
+++ src/components/graphs/Map/drawMap.tsx
@@ -1,17 +1,17 @@
 import * as d3 from "d3";
-import { GeoJSONDataFeature } from "./map.types";
 
 const getGenerator = (
-  data: GeoJSONDataFeature,
+  data: d3.ExtendedFeatureCollection,
   dimensions: [number, number]
 ) => {
+  // TODO: Correct the fact that the GeoJSON has the map frame included
   const projection = d3.geoMercator().fitSize(dimensions, data);
   const generator = d3.geoPath().projection(projection);
   return generator;
 };
 
 export const drawMap = (
-  data: GeoJSONDataFeature,
+  data: d3.ExtendedFeatureCollection,
   parentRef: SVGSVGElement,
   dimensions: [number, number]
 ) => {
