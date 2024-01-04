810da41fc91a90b556301f1cd53b78c4e6e3f410
arturomp99 <arturo.m.p.99@gmail.com>
2024-01-04 17:36:52 +0100

    feat: APP style mainpage

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,5 +1,28 @@
 import React from "react";
+import { appPaddings, paragraphMargin } from "src/data/constants";
+import styled from "styled-components";
+
+const StyledSection = styled.section<{ color: string }>`
+  background-color: ${({ color }) => color};
+  padding: 0 ${appPaddings.lg};
+  overflow: hidden;
+`;
+
+const StyledTitle = styled.p`
+  margin: ${paragraphMargin} 0;
+  font-size: 50px;
+  color: #2d544a;
+  font-weight: bold;
+`;
 
 export const MainPage = () => {
-  return <h1>MAIN PAGE</h1>;
+  return (
+    <>
+      <StyledSection color={"#e5f3d6"}>
+        <StyledTitle>MAIN PAGE</StyledTitle>
+      </StyledSection>
+      <StyledSection color={"#faf5f2"}>Content</StyledSection>
+      <StyledSection color={"#2d544a"}>Content</StyledSection>
+    </>
+  );
 };
