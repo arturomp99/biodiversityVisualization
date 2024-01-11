604c277235829e9e5f2ee60791bbe97162f428f9
arturomp99 <arturo.m.p.99@gmail.com>
2024-01-11 01:14:13 +0100

    feat-TIMELINE-read-complex-csv

--- src/components/shared/hooks/useReadData.tsx
+++ src/components/shared/hooks/useReadData.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
-import { DSVRowString, ExtendedFeatureCollection, csv, json } from "d3";
+import { DSVRowString, ExtendedFeatureCollection, csv, json, dsv } from "d3";
 import { TreeDataType } from "src/components/graphs/Dendrogram/dendrogram.types";
 import { SoundChartDataType } from "src/components/graphs/LineChart/lineChart.types";
 import { SoundHeaders } from "src/data/sampleData/sampleData.types";
 import { TemporalDataType } from "src/components/graphs/TimeLine/timeLine.types";
+import { CleanDataFileHeaders } from "src/data/data.types";
 
 const useReadDendrogramData = () => {
   const [data, setData] = useState<TreeDataType | undefined>(undefined);
@@ -108,11 +109,69 @@ const useReadTimeLineData = () => {
   return { data, loading };
 };
 
+const arrayProperties: Array<CleanDataFileHeaders> = [
+  "occurrenceID",
+  "basisOfRecord",
+  "eventDate",
+  "AI Detection Method/Model",
+  "Confidence%",
+  "Verification Method",
+  "Verification Name",
+  "individualCount",
+  "organismQuantity",
+  "organismQuantityType",
+  "decimalLatitude",
+  "decimalLongitude",
+  "geodeticDatum",
+  "coordinateUncertaintyInMeters",
+  "verbatimCoordinates",
+  "verbatimCoordinateSystem",
+  "occurrenceRemarks",
+  "references",
+];
+
+const useReadComplexData = () => {
+  const [data, setData] = useState<
+    Record<CleanDataFileHeaders, string | string[]>[] | undefined
+  >(undefined);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const readData = await dsv(
+          ";",
+          "/sampleData/clean.csv",
+          (dataEntry: DSVRowString<CleanDataFileHeaders>) => {
+            const cleanEntry: Record<CleanDataFileHeaders, string | string[]> =
+              dataEntry;
+            for (const property of arrayProperties) {
+              cleanEntry[property] = dataEntry[property].split(",") as string[];
+            }
+            return cleanEntry;
+          }
+        );
+        if (!readData) throw "empty data";
+        setLoading(false);
+        setData(Array.from(readData));
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } catch (error: any) {
+        return console.log("Error loading the data\n", error);
+      }
+    };
+    fetchData();
+  }, []);
+  return { data, loading };
+};
+
 export const useReadData = () => {
   const dendrogramData = useReadDendrogramData();
   const lineChartData = useReadLineChartData();
   const mapData = useReadMapData();
   const timeLineData = useReadTimeLineData();
+  const complexData = useReadComplexData();
 
-  return { dendrogramData, lineChartData, mapData, timeLineData };
+  console.log("complexData", complexData);
+
+  return { dendrogramData, lineChartData, mapData, timeLineData, complexData };
 };
