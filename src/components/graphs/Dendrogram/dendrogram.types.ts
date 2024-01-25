32a853092bcdffbc62016b5c8119408d9c1ade01
arturomp99 <arturo.m.p.99@gmail.com>
2024-01-25 19:14:52 +0100

    feat-DENDROGRAM-first-collapse-transition

--- src/components/graphs/Dendrogram/dendrogram.types.ts
+++ src/components/graphs/Dendrogram/dendrogram.types.ts
@@ -1,6 +1,47 @@
 import { InternMap } from "d3";
 import { CleanDataFileHeaders } from "src/data/data.types";
 
+export interface TreeNode<T> {
+  new (data: T): this;
+  x: number;
+  y: number;
+  x0?: number;
+  y0?: number;
+  children?: this[];
+  expanded?: boolean;
+  childrenNodes?: SVGGElement[];
+  action?: "collapse" | "expand" | undefined;
+  data: T;
+  readonly depth: number;
+  readonly height: number;
+  parent: this | null;
+  readonly value?: number | undefined;
+  readonly id?: string | undefined;
+  ancestors(): this[];
+  descendants(): this[];
+  leaves(): this[];
+  find(filter: (node: this) => boolean): this | undefined;
+  path(target: this): this[];
+  links(): Array<d3.HierarchyLink<T>>;
+  sum(value: (d: T) => number): this;
+  count(): this;
+  sort(compare: (a: this, b: this) => number): this;
+  [Symbol.iterator](): Iterator<this>;
+  each<T = undefined>(
+    func: (this: T, node: this, index: number, thisNode: this) => void,
+    that?: T
+  ): this;
+  eachAfter<T = undefined>(
+    func: (this: T, node: this, index: number, thisNode: this) => void,
+    that?: T
+  ): this;
+  eachBefore<T = undefined>(
+    func: (this: T, node: this, index: number, thisNode: this) => void,
+    that?: T
+  ): this;
+  copy(): this;
+}
+
 export type TreeDataType = InternMap<
   string | string[],
   InternMap<
@@ -24,7 +65,3 @@ export type TreeDataType = InternMap<
     >
   >
 >;
-
-// export interface DendrogramChartDataType extends TreeDataType {
-//   initialPosition: Point;
-// }
--- src/components/graphs/Dendrogram/drawDendrogram.tsx
+++ src/components/graphs/Dendrogram/drawDendrogram.tsx
@@ -1,42 +1,165 @@
 import * as d3 from "d3";
-import { TreeDataType } from "./dendrogram.types";
+import { TreeDataType, TreeNode } from "./dendrogram.types";
 import { dendrogramParameters, graphMargin } from "src/data/constants";
 import { dendrogramClassNames } from "src/data/idClassNames";
 import { Point, verticalDiagonalLine } from "src/utils/lineEquations";
 
+const setInitialState = (root: TreeNode<TreeDataType>) => {
+  if (!root.children) return;
+  root.x0 = root.x;
+  root.y0 = root.y;
+  root.expanded = true;
+  root.children.forEach((descendantNode) => {
+    if (descendantNode.expanded) {
+      setInitialState(descendantNode);
+    } else {
+      descendantNode.x0 = root.x0;
+      descendantNode.y0 = root.y0;
+    }
+  });
+};
+
+const collapseTransition = (
+  selection: d3.Selection<
+    SVGGElement,
+    TreeNode<TreeDataType>,
+    SVGSVGElement,
+    unknown
+  >,
+  parentCoordinates: [number, number]
+) => {
+  console.log("parentCoordinates", parentCoordinates.toString());
+  selection
+    .filter((dataPoint) => dataPoint.action === "collapse")
+    .each((dataPoint) => (dataPoint.action = undefined))
+    .transition()
+    .duration(500)
+    .attr("transform", `translate(${parentCoordinates.toString()})`);
+};
+
+// const expandTransition = (
+//   selection: d3.Selection<
+//     SVGGElement,
+//     TreeNode<TreeDataType>,
+//     SVGSVGElement,
+//     unknown
+//   >
+// ) => {
+//   selection
+//     .filter((dataPoint) => dataPoint.action === "expand")
+//     .each((dataPoint) => (dataPoint.action = undefined))
+//     .transition()
+//     .duration(500)
+//     .attr("transform", "translate(150,150)");
+// };
+
+/**
+ *
+ * @param node
+ * @param nodeElement
+ * @returns void
+ * Collapses a certain node of the tree,
+ * making sure that its children are also collapsed
+ */
+const collapseNode = (node: TreeNode<TreeDataType>): void => {
+  node.children &&
+    node.children.forEach((childNode) => {
+      childNode.action = "collapse";
+      childNode.children && collapseNode(childNode);
+    });
+  return;
+};
+
+/**
+ *
+ * @param node
+ * @param nodeElement
+ * @returns void
+ * Expands a certain node in the tree,
+ * making sure that its children keep their state:
+ * - If they were collapsed they remain collapsed
+ * - If they were expanded, they remain expanded
+ */
+const expandNode = (
+  node: TreeNode<TreeDataType>,
+  nodeElement?: SVGSVGElement
+): void => {
+  if (nodeElement) console.log("ELEMENT CLICKED", nodeElement);
+  if (!node.childrenNodes || !node.children || !node.expanded) return;
+
+  node.children.forEach((childNode) => expandNode(childNode));
+  node.childrenNodes.forEach((childNode) => {
+    d3.select(childNode).attr("fill", "blue");
+  });
+  return;
+};
+
 export const scaleData = (data: TreeDataType, dimensions: [number, number]) => {
   const [dendrogramWidth, dendrogramHeight] = dimensions;
-  // const root = d3.hierarchy({
-  //   ...data,
-  //   initialPosition: {
-  //     x: 0,
-  //     y: 0,
-  //   },
-  // });
-  const root = d3.hierarchy(data);
-  return d3
+
+  const root = d3.hierarchy<TreeDataType>(data);
+  root.count(); // Counts the num of leaves on each branch => Populating the value property on each node
+
+  const treeStructure = d3
     .tree<TreeDataType>()
     .nodeSize([12, 12])
-    .size([dendrogramWidth, dendrogramHeight])(root);
+    .size([dendrogramWidth, dendrogramHeight])(root) as TreeNode<TreeDataType>;
+
+  setInitialState(treeStructure);
+  console.log("treeStructure", treeStructure);
+  return treeStructure;
 };
 
 export const drawDendrogram = (
-  data: d3.HierarchyPointNode<TreeDataType>,
+  data: TreeNode<TreeDataType>,
   parentRef: SVGSVGElement
 ) => {
   const dendrogramMarkers = d3
     .select(parentRef)
-    .selectAll(`${dendrogramClassNames.markerGroup}`)
-    .data(data.descendants())
-    .join("g")
+    .selectAll<typeof parentRef, typeof data>(
+      `${dendrogramClassNames.markerGroup}`
+    )
+    .data(
+      data.descendants(),
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      (dataPoint: any) => dataPoint.data.species || dataPoint.data[0] || "root"
+    )
+    .enter()
+    .append("g")
     .attr("class", `${dendrogramClassNames.markerGroup}`)
+    .attr(
+      "id",
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      (dataPoint: any) => dataPoint.data.species || dataPoint.data[0] || "root"
+    )
     .attr(
       "transform",
       (dataPoint) =>
         `translate(${dataPoint.x + graphMargin.left}, ${
           dataPoint.y + graphMargin.top
         })`
-    );
+    )
+    .each((dataPoint, index, nodesGroup) => {
+      if (!dataPoint.parent) return;
+      dataPoint.parent.childrenNodes = [
+        ...(dataPoint.parent?.childrenNodes || []),
+        nodesGroup[index],
+      ];
+    });
+  dendrogramMarkers.on("click", (event, dataPoint) => {
+    if (!dataPoint.children) {
+      console.log("CLICKED A LEAF");
+      return;
+    }
+    if (dataPoint.expanded) {
+      dataPoint.expanded = false;
+      collapseNode(dataPoint);
+      collapseTransition(dendrogramMarkers, [dataPoint.x, dataPoint.y]);
+      return;
+    }
+    dataPoint.expanded = true;
+    expandNode(dataPoint, event.target);
+  });
 
   dendrogramMarkers
     .selectAll(`.${dendrogramClassNames.markerNode}`)
