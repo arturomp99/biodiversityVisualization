06beae3df7f8a41df3fbf47aa33144b4c59fa6be
arturomp99 <arturo.m.p.99@gmail.com>
2024-02-06 00:32:28 +0100

    fix: DENDROGRAM nodes spacing

--- src/components/graphs/shared/Interactivity/zoom.ts
+++ src/components/graphs/shared/Interactivity/zoom.ts
@@ -1,4 +1,7 @@
 import * as d3 from "d3";
+import { dendrogramClassNames } from "src/data/idClassNames";
+import { TreeDataType, TreeNode } from "../../Dendrogram/dendrogram.types";
+import { dendrogramParameters } from "src/data/constants";
 
 export const addZoom = (
   svg: SVGSVGElement,
@@ -6,15 +9,40 @@ export const addZoom = (
   extent: [number, number]
 ) => {
   const handleZoom = (event: d3.D3ZoomEvent<SVGSVGElement, unknown>) => {
-    return d3
-      .select(zoomContainer)
-      .attr("transform", event.transform.toString());
+    d3.select(zoomContainer).attr("transform", event.transform.toString());
+  };
+
+  const handleZoomEnd = (event: d3.D3ZoomEvent<SVGSVGElement, unknown>) => {
+    const transition = d3
+      .selectAll<SVGGElement, TreeNode<TreeDataType>>(
+        `.${dendrogramClassNames.markerGroup}`
+      )
+      .filter((dataPoint) => !dataPoint?.parent || !!dataPoint.parent.expanded)
+      .transition()
+      .duration(dendrogramParameters.transitions.zoomEndTransitionDuration);
+
+    transition
+      .selectChild(`.${dendrogramClassNames.markerNode}`)
+      .attr(
+        "r",
+        dendrogramParameters.nodeParameters.radius / event.transform.k
+      );
+
+    transition
+      .selectChild(`.${dendrogramClassNames.markerLink}`)
+      .attr(
+        "stroke-width",
+        `${
+          dendrogramParameters.linkParameters.strokeWidth / event.transform.k
+        }px`
+      ); // Modify strokeWidth to default one
   };
 
   const zoom = d3
     .zoom<SVGSVGElement, unknown>()
     .scaleExtent(extent)
-    .on("zoom", handleZoom);
+    .on("zoom", handleZoom)
+    .on("end", handleZoomEnd);
 
   d3.select(svg).call(zoom);
 };
