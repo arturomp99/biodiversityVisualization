4ab246c6b84cc78633bc97370eec7bd6160d1055
arturomp99 <arturo.m.p.99@gmail.com>
2024-02-05 02:10:23 +0100

    refactor: DENDROGRAM click interactivity

--- src/components/graphs/Dendrogram/drawDendrogram.tsx
+++ src/components/graphs/Dendrogram/drawDendrogram.tsx
@@ -18,133 +18,6 @@ const setInitialState = (
       );
 };
 
-const collapseTransition = (
-  selection: d3.Selection<
-    SVGGElement,
-    TreeNode<TreeDataType>,
-    SVGSVGElement,
-    unknown
-  >,
-  parentCoordinates: [number, number]
-) => {
-  const transition = selection
-    .filter((dataPoint) => dataPoint.action === "collapse")
-    .each((dataPoint) => (dataPoint.action = undefined))
-    .transition()
-    .duration(dendrogramParameters.transitions.collapseDuration)
-    .attr(
-      "transform",
-      `translate(${parentCoordinates.toString()}) translate(${
-        graphMargin.left
-      } ${graphMargin.top})`
-    );
-
-  transition
-    .selectChild(`.${dendrogramClassNames.markerNode}`)
-    .attr("r", dendrogramParameters.nodeParameters.radiusCollapsed);
-
-  transition
-    .selectChild<SVGSVGElement, TreeNode<TreeDataType>>(
-      `.${dendrogramClassNames.markerLink}`
-    )
-    .attr("d", (dataPoint) => {
-      if (!dataPoint.parent) return null;
-      return verticalDiagonalLine(
-        { x: 0, y: 0 },
-        {
-          x: 0,
-          y: 0,
-        }
-      );
-    });
-};
-
-const expandTransition = (
-  selection: d3.Selection<
-    SVGGElement,
-    TreeNode<TreeDataType>,
-    SVGSVGElement,
-    unknown
-  >
-) => {
-  const transition = selection
-    .filter((dataPoint) => dataPoint.action === "expand")
-    .each((dataPoint) => (dataPoint.action = undefined))
-    .transition()
-    .duration(dendrogramParameters.transitions.collapseDuration)
-    .attr(
-      "transform",
-      (dataPoint) =>
-        `translate(${dataPoint.x},${dataPoint.y}) translate(${graphMargin.left} ${graphMargin.top})`
-    );
-
-  transition
-    .selectChild(`.${dendrogramClassNames.markerNode}`)
-    .attr("r", dendrogramParameters.nodeParameters.radius);
-
-  transition
-    .selectChild<SVGSVGElement, TreeNode<TreeDataType>>(
-      `.${dendrogramClassNames.markerLink}`
-    )
-    .attr("d", (dataPoint) => {
-      if (!dataPoint.parent) return null;
-      return verticalDiagonalLine(
-        { x: 0, y: 0 },
-        {
-          x: dataPoint.parent.x - dataPoint.x,
-          y: dataPoint.parent.y - dataPoint.y,
-        }
-      );
-    });
-
-  transition.on(
-    "end",
-    (dataPoint) =>
-      dataPoint.childrenNodes &&
-      dataPoint.childrenNodes.forEach((childNode) => {
-        d3.select(childNode).attr(
-          "transform",
-          `translate(${dataPoint.x},${dataPoint.y}) translate(${graphMargin.left} ${graphMargin.top})`
-        );
-      })
-  );
-};
-
-/**
- *
- * @param node
- * @param nodeElement
- * @returns void
- * Collapses a certain node of the tree,
- * making sure that its children are also collapsed
- */
-const collapseNode = (node: TreeNode<TreeDataType>): void => {
-  node.children &&
-    node.children.forEach((childNode) => {
-      childNode.action = "collapse";
-      childNode.children && collapseNode(childNode);
-    });
-};
-
-/**
- *
- * @param node
- * @param nodeElement
- * @returns void
- * Expands a certain node in the tree,
- * making sure that its children keep their state:
- * - If they were collapsed they remain collapsed
- * - If they were expanded, they remain expanded
- */
-const expandNode = (node: TreeNode<TreeDataType>): void => {
-  node.children &&
-    node.children.forEach((childNode) => {
-      childNode.action = "expand";
-      childNode.expanded && expandNode(childNode);
-    });
-  return;
-};
-
 export const scaleData = (data: TreeDataType, dimensions: [number, number]) => {
   const [dendrogramWidth, dendrogramHeight] = dimensions;
 
@@ -193,17 +66,9 @@ export const drawDendrogram = (
       return `translate(${position.x + graphMargin.left}, ${
         position.y + graphMargin.top
       })`;
-    })
-    .each((dataPoint, index, nodesGroup) => {
-      // We point to each child svg node from parent
-      if (!dataPoint.parent) return;
-      dataPoint.parent.childrenNodes = [
-        ...(dataPoint.parent?.childrenNodes || []),
-        nodesGroup[index],
-      ];
     });
 
-  const dendrogramNodes = dendrogramMarkers
+  dendrogramMarkers
     .selectAll<SVGSVGElement, TreeNode<TreeDataType>>(
       `.${dendrogramClassNames.markerNode}`
     )
@@ -216,21 +81,6 @@ export const drawDendrogram = (
         : dendrogramParameters.nodeParameters.radiusCollapsed
     );
 
-  dendrogramNodes.on("click", (_, dataPoint) => {
-    if (!dataPoint.children) {
-      console.log("CLICKED A LEAF"); // TODO: TRIGGER AN ACTION
-      return;
-    }
-    if (dataPoint.expanded) {
-      dataPoint.expanded = false;
-      collapseNode(dataPoint);
-      collapseTransition(dendrogramMarkers, [dataPoint.x, dataPoint.y]);
-      return;
-    }
-    dataPoint.expanded = true;
-    expandNode(dataPoint);
-    expandTransition(dendrogramMarkers);
-  });
   // dendrogramMarkers
   //   .selectAll(`.${dendrogramClassNames.markerLabel}`)
   //   .data((singleData) => [singleData])
