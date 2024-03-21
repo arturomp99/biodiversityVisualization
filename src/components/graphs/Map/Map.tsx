2f9560555a0fd99bc154b626b6555ebfb36e4075
arturomp99 <arturo.m.p.99@gmail.com>
2024-03-21 23:58:56 +0100

    feat: MAP migrate to leaflet

--- src/components/graphs/Map/Map.tsx
+++ src/components/graphs/Map/Map.tsx
@@ -1,72 +1,24 @@
-import React, { FC, useEffect, createRef, useState } from "react";
+import React, { FC, useEffect, createRef } from "react";
+import L from "leaflet";
 import { GraphProps } from "../graphsProps.types";
-import { drawMapMarkers, drawMap } from "./drawMap";
 import { StyledMapContainer } from "./styles";
 import { mapIdNames } from "src/data/idClassNames";
-import { addZoom } from "../shared/Interactivity/zoom/zoom";
-import { mapChartParameters, resizeTimeout } from "src/data/constants";
-import { useDataContext } from "src/contexts/dataContext";
-import { getDimensionsWithoutMargin } from "src/utils/getDimensionsWithoutMargin";
-import { createMapTooltip } from "./interactivity/createMapTooltip";
 
-export const Map: FC<GraphProps> = ({ isBasicInteractive, dimensions }) => {
-  const {
-    mapData: map,
-    sensorsData: { data, loading },
-  } = useDataContext();
-  const [projection, setProjection] = useState<d3.GeoProjection | undefined>();
-  const node = createRef<SVGSVGElement>();
-  const zoomContainer = createRef<SVGSVGElement>();
-
-  const realDimensions = getDimensionsWithoutMargin(dimensions);
+export const Map: FC<GraphProps> = () => {
+  const node = createRef<HTMLDivElement>();
 
   useEffect(() => {
-    if (!map.data || !zoomContainer.current) {
-      return;
-    }
+    if (!node.current) return;
+    const map = L.map(node.current).setView([1.3521, 103.8198], 13);
 
-    const { projection } = drawMap(map.data, zoomContainer.current, dimensions);
-    setProjection(() => projection);
-  }, [map.data]);
-
-  useEffect(() => {
-    if (!data || !zoomContainer.current || !projection) {
-      return;
-    }
-    drawMapMarkers(data, projection, zoomContainer.current);
-
-    if (isBasicInteractive) {
-      createMapTooltip(zoomContainer.current);
-    }
-  }, [projection, data]);
-
-  useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      if (map.loading || !realDimensions || !node.current) return;
-      // TODO: TRANSLATE MAP
-    }, resizeTimeout);
-
-    return () => {
-      clearTimeout(timeoutId);
-    };
-  }, [realDimensions]);
-
-  useEffect(() => {
-    if (!node.current || !zoomContainer.current) return;
-    if (isBasicInteractive) {
-      addZoom(node.current, zoomContainer.current, [
-        mapChartParameters.zoom.min,
-        mapChartParameters.zoom.max,
-      ]);
-    }
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution: "© OpenStreetMap contributors",
+    }).addTo(map);
   }, []);
 
   return (
     <>
-      {(map.loading || loading) && <div>LOADING MAP DATA...</div>}
-      <StyledMapContainer ref={node} id={`${mapIdNames.container}`}>
-        <g ref={zoomContainer} id={`${mapIdNames.zoomContainer}`} />
-      </StyledMapContainer>
+      <StyledMapContainer ref={node} id={`${mapIdNames.container}`} />
     </>
   );
 };
