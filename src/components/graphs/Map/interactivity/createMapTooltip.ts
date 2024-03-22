c3fc66ed1c7af27c8beea8799afa3fdcd0ed30ac
arturomp99 <arturo.m.p.99@gmail.com>
2024-03-23 00:30:56 +0100

    fix: MAP type errors

--- src/components/graphs/Map/interactivity/createMapTooltip.ts
+++ src/components/graphs/Map/interactivity/createMapTooltip.ts
@@ -1,26 +1,26 @@
-import * as d3 from "d3";
-import { mapClassNames, tooltipInteractiveClass } from "src/data/idClassNames";
-import tippy from "tippy.js";
-import { MapChartDataType } from "../../graphsData.types";
+// import * as d3 from "d3";
+// import { mapClassNames, tooltipInteractiveClass } from "src/data/idClassNames";
+// import tippy from "tippy.js";
+// import { MapChartDataType } from "../../graphsData.types";
 
-export const getTooltipContent = (data: MapChartDataType) => {
-  return `<p>sensor ${data.sensorId || ""}
-    <ul> 
-      <li>lat: ${data.latitude}</li>
-      <li>lon: ${data.longitude}</li>
-      <li>observations: ${data.observationsNum}</li>
-    </ul>
-  </p>`;
-};
+// export const getTooltipContent = (data: MapChartDataType) => {
+//   return `<p>sensor ${data.sensorId || ""}
+//     <ul>
+//       <li>lat: ${data.latitude}</li>
+//       <li>lon: ${data.longitude}</li>
+//       <li>observations: ${data.observationsNum}</li>
+//     </ul>
+//   </p>`;
+// };
 
-export const createMapTooltip = (parentRef: SVGSVGElement) => {
-  const markersSelection = d3
-    .select(parentRef)
-    .selectAll<SVGCircleElement, MapChartDataType>(
-      `.${mapClassNames.sensorMarker}`
-    )
-    .classed(`${tooltipInteractiveClass}`, true)
-    .attr("data-tippy-content", (dataPoint) => getTooltipContent(dataPoint));
+// export const createMapTooltip = (parentRef: SVGSVGElement) => {
+//   const markersSelection = d3
+//     .select(parentRef)
+//     .selectAll<SVGCircleElement, MapChartDataType>(
+//       `.${mapClassNames.sensorMarker}`
+//     )
+//     .classed(`${tooltipInteractiveClass}`, true)
+//     .attr("data-tippy-content", (dataPoint) => getTooltipContent(dataPoint));
 
-  tippy(markersSelection.nodes(), { allowHTML: true });
-};
+//   tippy(markersSelection.nodes(), { allowHTML: true });
+// };
