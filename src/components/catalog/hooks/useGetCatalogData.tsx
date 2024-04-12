e0d7245d25af9afc52170390340e5092f50b31df
arturomp99 <arturo.m.p.99@gmail.com>
2024-04-12 13:42:20 +0200

    feat: CATALOG filter data

--- src/components/catalog/hooks/useGetCatalogData.tsx
+++ src/components/catalog/hooks/useGetCatalogData.tsx
@@ -3,18 +3,29 @@ import config from "src/config.json";
 import { useFetch } from "src/components/shared/hooks/useReadData/useFetch";
 import { CatalogDataType, TotalCatalogInfoType } from "../types";
 import { catalogParameters } from "src/data/constants";
+import { useApplyFilters } from "src/components/shared/hooks/useApplyFilters/useApplyFilters";
 
 export const useGetCatalogData = () => {
   const [page, setPage] = useState(1);
   const [pageData, setPageData] = useState<CatalogDataType[]>();
-  const { data: totalData, loading: totalDataLoading } =
-    useFetch<TotalCatalogInfoType>(
-      config.BACKEND_URL + config.CATALOG_TOTAL_KEY
-    );
-
-  const { data, loading: pageDataLoading } = useFetch<CatalogDataType[]>(
-    config.BACKEND_URL + config.CATALOG_KEY
+  const {
+    data: totalData,
+    setData: setTotalData,
+    loading: totalDataLoading,
+  } = useFetch<TotalCatalogInfoType>(
+    config.BACKEND_URL + config.CATALOG_TOTAL_KEY
   );
+
+  const {
+    data,
+    dataRef,
+    setData,
+    loading: pageDataLoading,
+  } = useFetch<CatalogDataType[]>(config.BACKEND_URL + config.CATALOG_KEY);
+
+  useApplyFilters<NonNullable<typeof data>[0]>(dataRef.current, setData, () => {
+    setPage(1);
+  });
 
   useEffect(() => {
     setPageData(() =>
@@ -24,6 +35,15 @@ export const useGetCatalogData = () => {
       )
     );
   }, [page, data]);
+
+  useEffect(() => {
+    setTotalData(() => ({
+      totalAnimals: data?.length,
+      totalPages: Math.ceil(
+        (data?.length ?? 1) / catalogParameters.animalsPerPage
+      ),
+    }));
+  }, [data]);
 
   return {
     loading: totalDataLoading || pageDataLoading,
